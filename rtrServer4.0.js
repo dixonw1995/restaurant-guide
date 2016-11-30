c411dda7ca7814f3af75b37c93b9e8b43dadbeba
dixonw1995 <dixonw1995@gmail.com>
2016-12-01 02:38:52 +0800

    Add files via upload

--- rtrServer4.0.js
+++ rtrServer4.0.js
@@ -86,7 +86,7 @@ app.get("/register", function(req, res) {
 	res.render("register");
 });
 app.post("/register", function(req, res, next) {
-	var userid = req.body.userid;
+	req.userid = req.body.userid;
 	console.log(userid + " is registering")
 	//register
 	//if (connection) {
